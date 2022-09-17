9d3bb7c1df614ea4f62290f49780a178047d5e96
Dev-Trewin <rogertrewin.r@gmail.com>
2022-09-16 18:48:12 -0600

    added card component card style

--- monsterrolodex/src/components/Card-list/card-list.component.jsx
+++ monsterrolodex/src/components/Card-list/card-list.component.jsx
@@ -1,21 +1,22 @@
 import { Component } from "react";
-
+import './card-list.style.css'
+import Card from '../Card/Card.component'
 class Cardlist extends Component{
 
     render(){
     console.log('render from cardlist');
      const{monsters}=this.props;
-
+      
        return(
         <div className="card-list">
-  
-          {monsters.map(monster=>(
-           <div className='card-container' key={monster.id}>
-            <img alt={`monster ${monster.name}`} src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}/>
-            <h2>{monster.name}</h2>
-            <p>{monster.email}</p>
-           </div>
-          ))}
+         
+          {monsters.map((monster)=>{
+       
+           return (
+            <Card monsters={monster}/>
+           
+             )
+          })}
         </div>
        );
     }
